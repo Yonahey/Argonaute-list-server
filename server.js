263bac61b4633c7bfd3f2d62e67b9bea2a06e0dd
Yonahey <cherfi.ismael@gmail.comm>
2022-12-26 16:40:57 +0100

    creating server.js, handeling cors policy and connection to the DB

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ const {default: mongoose} = require("mongoose");
 const server = express();
 const cors = require("cors");
 
+const DbUri = "mongodb://localhost:27017/compagnons";
+server.use(express.json());
+
+server.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Credentials", true);
+	next();
+});
+
+server.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
+
+mongoose.connect(DbUri, {useNewUrlParser: true}).then(() => {
+	server.listen(5000, () => {
+		console.log("Server has started!");
+	});
+});
